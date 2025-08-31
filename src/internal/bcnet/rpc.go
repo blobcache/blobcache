829c418918c2416281aa1aba1bc84f87bffa1a4c
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2025-08-31 17:01:07 -0400

    bcnet: expose rpc functions

---
+++ src/internal/bcnet/rpc.go
@@ -0,0 +1,246 @@
+package bcnet
+
+import (
+	"context"
+	"fmt"
+
+	"blobcache.io/blobcache/src/blobcache"
+)
+
+type Transport interface {
+	Tell(ctx context.Context, ep blobcache.Endpoint, req *Message) error
+	Ask(ctx context.Context, ep blobcache.Endpoint, req Message, resp *Message) error
+}
+
+func Drop(ctx context.Context, tp Transport, ep blobcache.Endpoint, h blobcache.Handle) error {
+	var resp DropResp
+	if _, err := doAsk(ctx, tp, ep, MT_HANDLE_DROP, DropReq{Handle: h}, &resp); err != nil {
+		return err
+	}
+	return nil
+}
+
+func KeepAlive(ctx context.Context, tp Transport, ep blobcache.Endpoint, hs []blobcache.Handle) error {
+	var resp KeepAliveResp
+	if _, err := doAsk(ctx, tp, ep, MT_HANDLE_KEEP_ALIVE, KeepAliveReq{Handles: hs}, &resp); err != nil {
+		return err
+	}
+	return nil
+}
+
+func InspectHandle(ctx context.Context, tp Transport, ep blobcache.Endpoint, h blobcache.Handle) (*blobcache.HandleInfo, error) {
+	var resp InspectHandleResp
+	if _, err := doAsk(ctx, tp, ep, MT_HANDLE_INSPECT, InspectHandleReq{Handle: h}, &resp); err != nil {
+		return nil, err
+	}
+	return &resp.Info, nil
+}
+
+func OpenAs(ctx context.Context, tp Transport, ep blobcache.Endpoint, caller *blobcache.PeerID, target blobcache.OID, mask blobcache.ActionSet) (*blobcache.Handle, error) {
+	var resp OpenAsResp
+	if _, err := doAsk(ctx, tp, ep, MT_OPEN_AS, OpenAsReq{Target: target, Mask: mask}, &resp); err != nil {
+		return nil, err
+	}
+	return &resp.Handle, nil
+}
+
+func OpenFrom(ctx context.Context, tp Transport, ep blobcache.Endpoint, base blobcache.Handle, target blobcache.OID, mask blobcache.ActionSet) (*blobcache.Handle, error) {
+	var resp OpenFromResp
+	if _, err := doAsk(ctx, tp, ep, MT_OPEN_FROM, OpenFromReq{Base: base, Target: target, Mask: mask}, &resp); err != nil {
+		return nil, err
+	}
+	return &resp.Handle, nil
+}
+
+func Await(ctx context.Context, tp Transport, ep blobcache.Endpoint, cond blobcache.Conditions) error {
+	var resp AwaitResp
+	if _, err := doAsk(ctx, tp, ep, MT_VOLUME_AWAIT, AwaitReq{Cond: cond}, &resp); err != nil {
+		return err
+	}
+	return nil
+}
+
+func CreateVolume(ctx context.Context, tp Transport, ep blobcache.Endpoint, caller *blobcache.PeerID, vspec blobcache.VolumeSpec) (*blobcache.Handle, error) {
+	var resp CreateVolumeResp
+	if _, err := doAsk(ctx, tp, ep, MT_CREATE_VOLUME, CreateVolumeReq{Spec: vspec}, &resp); err != nil {
+		return nil, err
+	}
+	return &resp.Handle, nil
+}
+
+func BeginTx(ctx context.Context, tp Transport, ep blobcache.Endpoint, volh blobcache.Handle, txp blobcache.TxParams) (*blobcache.Handle, error) {
+	var resp BeginTxResp
+	if _, err := doAsk(ctx, tp, ep, MT_VOLUME_BEGIN_TX, BeginTxReq{Volume: volh, Params: txp}, &resp); err != nil {
+		return nil, err
+	}
+	return &resp.Tx, nil
+}
+
+func InspectTx(ctx context.Context, tp Transport, ep blobcache.Endpoint, tx blobcache.Handle) (*blobcache.TxInfo, error) {
+	var resp InspectTxResp
+	if _, err := doAsk(ctx, tp, ep, MT_TX_INSPECT, InspectTxReq{Tx: tx}, &resp); err != nil {
+		return nil, err
+	}
+	return &resp.Info, nil
+}
+
+func Commit(ctx context.Context, tp Transport, ep blobcache.Endpoint, tx blobcache.Handle, root *[]byte) error {
+	var resp CommitResp
+	if _, err := doAsk(ctx, tp, ep, MT_TX_COMMIT, CommitReq{Tx: tx, Root: root}, &resp); err != nil {
+		return err
+	}
+	return nil
+}
+
+func Abort(ctx context.Context, tp Transport, ep blobcache.Endpoint, tx blobcache.Handle) error {
+	var resp AbortResp
+	if _, err := doAsk(ctx, tp, ep, MT_TX_ABORT, AbortReq{Tx: tx}, &resp); err != nil {
+		return err
+	}
+	return nil
+}
+
+func Load(ctx context.Context, tp Transport, ep blobcache.Endpoint, tx blobcache.Handle, dst *[]byte) error {
+	var resp LoadResp
+	if _, err := doAsk(ctx, tp, ep, MT_TX_LOAD, LoadReq{Tx: tx}, &resp); err != nil {
+		return err
+	}
+	return nil
+}
+
+func Save(ctx context.Context, tp Transport, ep blobcache.Endpoint, tx blobcache.Handle, src []byte) error {
+	var resp SaveResp
+	if _, err := doAsk(ctx, tp, ep, MT_TX_SAVE, SaveReq{Tx: tx, Root: src}, &resp); err != nil {
+		return err
+	}
+	return nil
+}
+
+func Post(ctx context.Context, tp Transport, ep blobcache.Endpoint, txh blobcache.Handle, salt *blobcache.CID, data []byte) (blobcache.CID, error) {
+	var body []byte
+	body = append(body, txh.OID[:]...)
+	body = append(body, txh.Secret[:]...)
+	reqMsg := Message{}
+	if salt != nil {
+		// if there is a salt, write that befor the data.  The salt size is known.
+		reqMsg.SetCode(MT_TX_POST_SALT)
+		body = append(body, salt[:]...)
+	} else {
+		// just write the data.
+		reqMsg.SetCode(MT_TX_POST)
+	}
+	body = append(body, data[:]...)
+	reqMsg.SetBody(body)
+
+	// do request/response
+	var respMsg Message
+	if err := tp.Ask(ctx, ep, reqMsg, &respMsg); err != nil {
+		return blobcache.CID{}, err
+	}
+
+	if respMsg.Header().Code().IsError() {
+		return blobcache.CID{}, ParseWireError(respMsg.Header().Code(), respMsg.Body())
+	}
+	if !respMsg.Header().Code().IsOK() {
+		return blobcache.CID{}, fmt.Errorf("reply message has non-OK code: %d", respMsg.Header().Code())
+	}
+
+	// request is ok at this point.
+	respBody := respMsg.Body()
+	if len(respBody) != blobcache.CIDSize {
+		return blobcache.CID{}, fmt.Errorf("invalid response body length: %d", len(respBody))
+	}
+	var theirCID blobcache.CID
+	copy(theirCID[:], respBody)
+	return theirCID, nil
+}
+
+func Get(ctx context.Context, tp Transport, ep blobcache.Endpoint, txh blobcache.Handle, hf blobcache.HashFunc, cid blobcache.CID, salt *blobcache.CID, buf []byte) (int, error) {
+	var body []byte
+	body = append(body, txh.OID[:]...)
+	body = append(body, txh.Secret[:]...)
+	body = append(body, cid[:]...)
+	var reqMsg Message
+	reqMsg.SetCode(MT_TX_GET)
+	reqMsg.SetBody(body)
+
+	var respMsg Message
+	if err := tp.Ask(ctx, ep, reqMsg, &respMsg); err != nil {
+		return 0, err
+	}
+	if respMsg.Header().Code().IsError() {
+		return 0, ParseWireError(respMsg.Header().Code(), respMsg.Body())
+	}
+	if !respMsg.Header().Code().IsOK() {
+		return 0, fmt.Errorf("reply message has non-OK code: %d", respMsg.Header().Code())
+	}
+	respBody := respMsg.Body()
+	if err := blobcache.CheckBlob(hf, salt, &cid, respBody); err != nil {
+		return 0, err
+	}
+	if len(respMsg.Body()) > len(buf) {
+		return 0, fmt.Errorf("buffer too short")
+	}
+	copy(buf, respMsg.Body())
+	return len(respMsg.Body()), nil
+}
+
+func Exists(ctx context.Context, tp Transport, ep blobcache.Endpoint, tx blobcache.Handle, cids []blobcache.CID) (bool, error) {
+	var resp ExistsResp
+	if _, err := doAsk(ctx, tp, ep, MT_TX_EXISTS, ExistsReq{Tx: tx, CIDs: cids}, &resp); err != nil {
+		return false, err
+	}
+	return resp.Exists[0], nil
+}
+
+func Delete(ctx context.Context, tp Transport, ep blobcache.Endpoint, tx blobcache.Handle, cid blobcache.CID) error {
+	var resp DeleteResp
+	if _, err := doAsk(ctx, tp, ep, MT_TX_DELETE, DeleteReq{Tx: tx, CID: cid}, &resp); err != nil {
+		return err
+	}
+	return nil
+}
+
+func AllowLink(ctx context.Context, tp Transport, ep blobcache.Endpoint, tx blobcache.Handle, subvol blobcache.Handle) error {
+	var resp AllowLinkResp
+	if _, err := doAsk(ctx, tp, ep, MT_TX_ALLOW_LINK, AllowLinkReq{Tx: tx, Subvol: subvol}, &resp); err != nil {
+		return err
+	}
+	return nil
+}
+
+func doAsk[Req interface{ Marshal(out []byte) []byte }, Resp interface{ Unmarshal(data []byte) error }](ctx context.Context, node Transport, remote blobcache.Endpoint, code MessageType, req Req, zeroResp Resp) (Resp, error) {
+	reqData := req.Marshal(nil)
+	var reqMsg Message
+	reqMsg.SetCode(code)
+	reqMsg.SetBody(reqData)
+	var respMsg Message
+	if err := node.Ask(ctx, remote, reqMsg, &respMsg); err != nil {
+		var zero Resp
+		return zero, err
+	}
+	if respMsg.Header().Code().IsError() {
+		var zero Resp
+		return zero, ParseWireError(respMsg.Header().Code(), respMsg.Body())
+	}
+	if !respMsg.Header().Code().IsOK() {
+		var zero Resp
+		return zero, fmt.Errorf("reply message has non-OK code: %d", respMsg.Header().Code())
+	}
+	resp := zeroResp
+	if err := resp.Unmarshal(respMsg.Body()); err != nil {
+		var zero Resp
+		return zero, err
+	}
+	return resp, nil
+}
+
+func InspectVolume(ctx context.Context, tp Transport, ep blobcache.Endpoint, vol blobcache.Handle) (*blobcache.VolumeInfo, error) {
+	resp, err := doAsk(ctx, tp, ep, MT_VOLUME_INSPECT, InspectVolumeReq{
+		Volume: vol,
+	}, &InspectVolumeResp{})
+	if err != nil {
+		return nil, err
+	}
+	return &resp.Info, nil
+}
--- src/internal/bcnet/volume.go
+++ src/internal/bcnet/volume.go
@@ -15,15 +15,15 @@ var (
 
 // Volume is a remote volume.
 type Volume struct {
-	n    *Node
+	n    Transport
 	ep   blobcache.Endpoint
 	h    blobcache.Handle
 	info *blobcache.VolumeInfo
 }
 
-func NewVolume(n *Node, ep blobcache.Endpoint, h blobcache.Handle, info *blobcache.VolumeInfo) *Volume {
+func NewVolume(node Transport, ep blobcache.Endpoint, h blobcache.Handle, info *blobcache.VolumeInfo) *Volume {
 	return &Volume{
-		n:    n,
+		n:    node,
 		ep:   ep,
 		h:    h,
 		info: info,
@@ -43,13 +43,13 @@ func (v *Volume) Info() *blobcache.VolumeInfo {
 }
 
 func (v *Volume) Await(ctx context.Context, prev []byte, next *[]byte) error {
-	_, err := doBinary(ctx, v.n, v.ep, MT_VOLUME_AWAIT, AwaitReq{
+	_, err := doAsk(ctx, v.n, v.ep, MT_VOLUME_AWAIT, AwaitReq{
 		Cond: blobcache.Conditions{},
 	}, &AwaitResp{})
 	if err != nil {
 		return err
 	}
-	loadResp, err := doBinary(ctx, v.n, v.ep, MT_TX_LOAD, LoadReq{
+	loadResp, err := doAsk(ctx, v.n, v.ep, MT_TX_LOAD, LoadReq{
 		Tx: v.h,
 	}, &LoadResp{})
 	if err != nil {
@@ -60,7 +60,7 @@ func (v *Volume) Await(ctx context.Context, prev []byte, next *[]byte) error {
 }
 
 func (v *Volume) BeginTx(ctx context.Context, spec blobcache.TxParams) (volumes.Tx, error) {
-	resp, err := doBinary(ctx, v.n, v.ep, MT_VOLUME_BEGIN_TX, BeginTxReq{
+	resp, err := doAsk(ctx, v.n, v.ep, MT_VOLUME_BEGIN_TX, BeginTxReq{
 		Volume: v.h,
 		Params: spec,
 	}, &BeginTxResp{})
@@ -78,7 +78,7 @@ func (v *Volume) BeginTx(ctx context.Context, spec blobcache.TxParams) (volumes.
 
 // Tx is a transaction on a remote volume.
 type Tx struct {
-	n       *Node
+	n       Transport
 	ep      blobcache.Endpoint
 	h       blobcache.Handle
 	params  blobcache.TxParams
@@ -104,7 +104,7 @@ func (tx *Tx) Commit(ctx context.Context) error {
 	if tx.root != nil {
 		root = &tx.root
 	}
-	_, err := doBinary(ctx, tx.n, tx.ep, MT_TX_COMMIT, CommitReq{
+	_, err := doAsk(ctx, tx.n, tx.ep, MT_TX_COMMIT, CommitReq{
 		Tx:   tx.h,
 		Root: root,
 	}, &CommitResp{})
@@ -115,14 +115,14 @@ func (tx *Tx) Commit(ctx context.Context) error {
 }
 
 func (tx *Tx) Abort(ctx context.Context) error {
-	_, err := doBinary(ctx, tx.n, tx.ep, MT_TX_ABORT, AbortReq{
+	_, err := doAsk(ctx, tx.n, tx.ep, MT_TX_ABORT, AbortReq{
 		Tx: tx.h,
 	}, &AbortResp{})
 	return err
 }
 
 func (tx *Tx) Load(ctx context.Context, dst *[]byte) error {
-	resp, err := doBinary(ctx, tx.n, tx.ep, MT_TX_LOAD, LoadReq{
+	resp, err := doAsk(ctx, tx.n, tx.ep, MT_TX_LOAD, LoadReq{
 		Tx: tx.h,
 	}, &LoadResp{})
 	if err != nil {
@@ -143,41 +143,10 @@ func (tx *Tx) Save(ctx context.Context, src []byte) error {
 }
 
 func (tx *Tx) Post(ctx context.Context, salt *blobcache.CID, data []byte) (blobcache.CID, error) {
-	var body []byte
-	body = append(body, tx.h.OID[:]...)
-	body = append(body, tx.h.Secret[:]...)
-	reqMsg := Message{}
-	if salt != nil {
-		// if there is a salt, write that befor the data.  The salt size is known.
-		reqMsg.SetCode(MT_TX_POST_SALT)
-		body = append(body, salt[:]...)
-	} else {
-		// just write the data.
-		reqMsg.SetCode(MT_TX_POST)
-	}
-	body = append(body, data[:]...)
-	reqMsg.SetBody(body)
-
-	// do request/response
-	var respMsg Message
-	if err := tx.n.Ask(ctx, tx.ep, reqMsg, &respMsg); err != nil {
+	theirCID, err := Post(ctx, tx.n, tx.ep, tx.h, salt, data)
+	if err != nil {
 		return blobcache.CID{}, err
 	}
-
-	if respMsg.Header().Code().IsError() {
-		return blobcache.CID{}, ParseWireError(respMsg.Header().Code(), respMsg.Body())
-	}
-	if !respMsg.Header().Code().IsOK() {
-		return blobcache.CID{}, fmt.Errorf("reply message has non-OK code: %d", respMsg.Header().Code())
-	}
-
-	// request is ok at this point.
-	respBody := respMsg.Body()
-	if len(respBody) != blobcache.CIDSize {
-		return blobcache.CID{}, fmt.Errorf("invalid response body length: %d", len(respBody))
-	}
-	var theirCID blobcache.CID
-	copy(theirCID[:], respBody)
 	ourCID := tx.Hash(salt, data)
 	if theirCID != ourCID {
 		return blobcache.CID{}, fmt.Errorf("hash mismatch: ourCID=%s, theirCID=%s", ourCID, theirCID)
@@ -186,37 +155,11 @@ func (tx *Tx) Post(ctx context.Context, salt *blobcache.CID, data []byte) (blobc
 }
 
 func (tx *Tx) Get(ctx context.Context, cid blobcache.CID, salt *blobcache.CID, buf []byte) (int, error) {
-	var body []byte
-	body = append(body, tx.h.OID[:]...)
-	body = append(body, tx.h.Secret[:]...)
-	body = append(body, cid[:]...)
-	var reqMsg Message
-	reqMsg.SetCode(MT_TX_GET)
-	reqMsg.SetBody(body)
-
-	var respMsg Message
-	if err := tx.n.Ask(ctx, tx.ep, reqMsg, &respMsg); err != nil {
-		return 0, err
-	}
-	if respMsg.Header().Code().IsError() {
-		return 0, ParseWireError(respMsg.Header().Code(), respMsg.Body())
-	}
-	if !respMsg.Header().Code().IsOK() {
-		return 0, fmt.Errorf("reply message has non-OK code: %d", respMsg.Header().Code())
-	}
-	respBody := respMsg.Body()
-	if err := blobcache.CheckBlob(tx.Hash, salt, &cid, respBody); err != nil {
-		return 0, err
-	}
-	if len(respMsg.Body()) > len(buf) {
-		return 0, fmt.Errorf("buffer too short")
-	}
-	copy(buf, respMsg.Body())
-	return len(respMsg.Body()), nil
+	return Get(ctx, tx.n, tx.ep, tx.h, tx.Hash, cid, salt, buf)
 }
 
 func (tx *Tx) Delete(ctx context.Context, cid blobcache.CID) error {
-	_, err := doBinary(ctx, tx.n, tx.ep, MT_TX_DELETE, DeleteReq{
+	_, err := doAsk(ctx, tx.n, tx.ep, MT_TX_DELETE, DeleteReq{
 		Tx:  tx.h,
 		CID: cid,
 	}, &DeleteResp{})
@@ -227,7 +170,7 @@ func (tx *Tx) Delete(ctx context.Context, cid blobcache.CID) error {
 }
 
 func (tx *Tx) Exists(ctx context.Context, cid blobcache.CID) (bool, error) {
-	resp, err := doBinary(ctx, tx.n, tx.ep, MT_TX_EXISTS, ExistsReq{
+	resp, err := doAsk(ctx, tx.n, tx.ep, MT_TX_EXISTS, ExistsReq{
 		Tx:   tx.h,
 		CIDs: []blobcache.CID{cid},
 	}, &ExistsResp{})
@@ -247,41 +190,15 @@ func (tx *Tx) Hash(salt *blobcache.CID, data []byte) blobcache.CID {
 }
 
 func (tx *Tx) AllowLink(ctx context.Context, subvol blobcache.Handle) error {
-	_, err := doBinary(ctx, tx.n, tx.ep, MT_TX_ALLOW_LINK, AllowLinkReq{
+	_, err := doAsk(ctx, tx.n, tx.ep, MT_TX_ALLOW_LINK, AllowLinkReq{
 		Tx:     tx.h,
 		Subvol: subvol,
 	}, &AllowLinkResp{})
 	return err
 }
 
-func doBinary[Req interface{ Marshal(out []byte) []byte }, Resp interface{ Unmarshal(data []byte) error }](ctx context.Context, node *Node, remote blobcache.Endpoint, code MessageType, req Req, zeroResp Resp) (Resp, error) {
-	reqData := req.Marshal(nil)
-	var reqMsg Message
-	reqMsg.SetCode(code)
-	reqMsg.SetBody(reqData)
-	var respMsg Message
-	if err := node.Ask(ctx, remote, reqMsg, &respMsg); err != nil {
-		var zero Resp
-		return zero, err
-	}
-	if respMsg.Header().Code().IsError() {
-		var zero Resp
-		return zero, ParseWireError(respMsg.Header().Code(), respMsg.Body())
-	}
-	if !respMsg.Header().Code().IsOK() {
-		var zero Resp
-		return zero, fmt.Errorf("reply message has non-OK code: %d", respMsg.Header().Code())
-	}
-	resp := zeroResp
-	if err := resp.Unmarshal(respMsg.Body()); err != nil {
-		var zero Resp
-		return zero, err
-	}
-	return resp, nil
-}
-
-func OpenVolumeFrom(ctx context.Context, n *Node, ep blobcache.Endpoint, base blobcache.Handle, target blobcache.OID, mask blobcache.ActionSet) (*Volume, error) {
-	resp, err := doBinary(ctx, n, ep, MT_OPEN_FROM, OpenFromReq{
+func OpenVolumeFrom(ctx context.Context, tp Transport, ep blobcache.Endpoint, base blobcache.Handle, target blobcache.OID, mask blobcache.ActionSet) (*Volume, error) {
+	resp, err := doAsk(ctx, tp, ep, MT_OPEN_FROM, OpenFromReq{
 		Base:   base,
 		Target: target,
 		Mask:   mask,
@@ -292,11 +209,11 @@ func OpenVolumeFrom(ctx context.Context, n *Node, ep blobcache.Endpoint, base bl
 	if err := resp.Info.HashAlgo.Validate(); err != nil {
 		return nil, err
 	}
-	return NewVolume(n, ep, resp.Handle, &resp.Info), nil
+	return NewVolume(tp, ep, resp.Handle, &resp.Info), nil
 }
 
-func OpenVolumeAs(ctx context.Context, n *Node, ep blobcache.Endpoint, target blobcache.OID, mask blobcache.ActionSet) (*Volume, error) {
-	resp, err := doBinary(ctx, n, ep, MT_OPEN_AS, OpenAsReq{
+func OpenVolumeAs(ctx context.Context, tp Transport, ep blobcache.Endpoint, target blobcache.OID, mask blobcache.ActionSet) (*Volume, error) {
+	resp, err := doAsk(ctx, tp, ep, MT_OPEN_AS, OpenAsReq{
 		Target: target,
 		Mask:   mask,
 	}, &OpenAsResp{})
@@ -306,15 +223,5 @@ func OpenVolumeAs(ctx context.Context, n *Node, ep blobcache.Endpoint, target bl
 	if err := resp.Info.HashAlgo.Validate(); err != nil {
 		return nil, err
 	}
-	return NewVolume(n, ep, resp.Handle, &resp.Info), nil
-}
-
-func InspectVolume(ctx context.Context, n *Node, ep blobcache.Endpoint, vol blobcache.Handle) (*blobcache.VolumeInfo, error) {
-	resp, err := doBinary(ctx, n, ep, MT_VOLUME_INSPECT, InspectVolumeReq{
-		Volume: vol,
-	}, &InspectVolumeResp{})
-	if err != nil {
-		return nil, err
-	}
-	return &resp.Info, nil
+	return NewVolume(tp, ep, resp.Handle, &resp.Info), nil
 }
