1a5964d967495fc42b864e6efc62a6905a55b1d5
Brendon Carroll <git@brendoncarroll.net>
2025-12-14 20:46:51 -0500

    blobcachecmd: git url-for

--- src/bcsdk/bcsdk.go
+++ src/bcsdk/bcsdk.go
@@ -68,3 +68,27 @@ func OpenURL(ctx context.Context, bc blobcache.Service, u blobcache.URL) (*blobc
 		})
 	}
 }
+
+// URLFor produces a URL for a Volume.
+// If the Volume is a remote Volume than the OID of the Volume on the remote Node
+// and the Endpoint of the remote Node will be included in the URL instead of the local Node.
+func URLFor(ctx context.Context, bc blobcache.Service, volh blobcache.Handle) (*blobcache.URL, error) {
+	vinfo, err := bc.InspectVolume(ctx, volh)
+	if err != nil {
+		return nil, err
+	}
+	host, err := bc.Endpoint(ctx)
+	if err != nil {
+		return nil, err
+	}
+	voloid := volh.OID
+	switch {
+	case vinfo.Backend.Remote != nil:
+		host = vinfo.Backend.Remote.Endpoint
+		voloid = vinfo.Backend.Remote.Volume
+	}
+	return &blobcache.URL{
+		Node: host.Peer,
+		Base: voloid,
+	}, nil
+}
