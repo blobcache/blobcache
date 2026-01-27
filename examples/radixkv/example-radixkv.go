ca3d87593fa41c6f4347f6f9c19a569ca5346cf8
Brendon Carroll <git@brendoncarroll.net>
2026-01-26 19:15:45 -0500

    radixkv: expose the internal trie implementation as `schema/radixkv` (#67)
    Adds the new radixkv package, and also includes an example in
    `examples/radixkv`.

---
+++ examples/radixkv/example-radixkv.go
@@ -0,0 +1,71 @@
+// This is a program which will connect to a Blobcache
+// according to BLOBCACHE_API.
+// It creates an ephemeral Volume, and then uses the radixkv schema to
+// insert 10 key-value pairs.
+// It then gets all those pairs by key and prints the retrieved values.
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	bcclient "blobcache.io/blobcache/client/go"
+	"blobcache.io/blobcache/src/blobcache"
+	"blobcache.io/blobcache/src/schema/radixkv"
+)
+
+func main() {
+	ctx := context.Background()
+	if err := run(ctx); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(ctx context.Context) error {
+	bc := bcclient.NewClientFromEnv()
+
+	// Create a Volume
+	vspec := blobcache.DefaultLocalSpec()
+	volh, err := bc.CreateVolume(ctx, nil, vspec)
+	if err != nil {
+		return err
+	}
+	// prepare a Machine, it holds configuration and caches
+	// for the operations we are going to perform.
+	kvmach := radixkv.New(new(blobcache.CID), vspec.Local.HashAlgo.HashFunc())
+
+	// Put some keys
+	if err := radixkv.Modify(ctx, bc, *volh, kvmach, func(tx *radixkv.Tx) error {
+		for i := range 10 {
+			s := fmt.Sprintf("%d", i)
+			key := []byte(s + "-key")
+			val := []byte(s + "-value")
+			if err := tx.Put(ctx, key, val); err != nil {
+				return err
+			}
+			log.Printf("PUT %q => %q", key, val)
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	// Get some keys
+	if err := radixkv.View(ctx, bc, *volh, kvmach, func(tx *radixkv.Tx) error {
+		for i := range 10 {
+			s := fmt.Sprintf("%d", i)
+			key := []byte(s + "-key")
+			var val []byte
+			if _, err := tx.Get(ctx, key, &val); err != nil {
+				return err
+			}
+			log.Printf("GET %q => %q", key, val)
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
